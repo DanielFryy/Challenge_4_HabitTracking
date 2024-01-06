987c7ab63941ed90ab4e55cf6b3ad553c1f3bddc
Daniel Freire <dgz1606@gmail.com>
2024-01-06 10:16:20 -0500

    Initial Commit

---
+++ Challenge_4_HabitTracking/Challenge_4_HabitTrackingApp.swift
@@ -0,0 +1,17 @@
+//
+//  Challenge_4_HabitTrackingApp.swift
+//  Challenge_4_HabitTracking
+//
+//  Created by Daniel Freire on 1/6/24.
+//
+
+import SwiftUI
+
+@main
+struct Challenge_4_HabitTrackingApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
